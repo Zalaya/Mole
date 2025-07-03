040bae3f516e27e818c7ec5cd33232be52cac120
Zalaya <enol.corzal@gmail.com>
2025-07-03 16:04:18 +0200

    enhancement: add a blacklist mode, the files that are in the blacklist file are excluded and not written in the output file

--- script.sh
+++ script.sh
@@ -2,34 +2,53 @@
 
 set -euo pipefail
 
-OUTPUT_FILE=output.txt
-PROJECT_ROOT=.
+output_file="output.txt"
+project_root="."
+blacklist_file=""
 
-while getopts "o:" opt; do
-  case $opt in
-    o) OUTPUT_FILE=$OPTARG ;;
+while getopts "o:b:i:" flag; do
+  case "${flag}" in
+    o) output_file="${OPTARG}" ;;
+    b) blacklist_file="${OPTARG}" ;;
     *) exit 1 ;;
   esac
 done
+
 shift $((OPTIND - 1))
 
-if [ $# -gt 1 ]; then exit 1; fi
-if [ $# -eq 1 ]; then PROJECT_ROOT=$1; fi
+if [ "$#" -gt 1 ]; then
+  exit 1
+elif [ "$#" -eq 1 ]; then
+  project_root="$1"
+fi
 
-> "$OUTPUT_FILE"
+prune_expressions=()
 
-mapfile -t files < <(find "$PROJECT_ROOT" -type f ! -path "$PROJECT_ROOT/$OUTPUT_FILE" | sort)
+if [ -n "$blacklist_file" ]; then
+  mapfile -t ignore_patterns < <(sed -e 's/^[[:space:]]\+//; s/[[:space:]]\+$//' -e '/^#/d' -e '/^$/d' -e 's@/*$@@' "$blacklist_file")
 
-for FILE_PATH in "${files[@]}"; do
-  FILE_PATH=${FILE_PATH//\\//}
-  RELATIVE_PATH=${FILE_PATH#"$PROJECT_ROOT"/}
+  for pattern in "${ignore_patterns[@]}"; do
+    pattern=${pattern#/}
+    prune_expressions+=( -path "${project_root}/${pattern}" -prune -o )
+  done
+fi
+
+: > "$output_file"
+
+find_arguments=( "$project_root" "${prune_expressions[@]}" -type f ! -path "${project_root}/${output_file}" -print )
+
+mapfile -t files < <(find "${find_arguments[@]}" | sort)
+
+for filepath in "${files[@]}"; do
+  filepath=${filepath//\\//}
+  relative_path=${filepath#"${project_root}/"}
 
   {
-    echo "File: $RELATIVE_PATH"
-    echo ""
-    cat "$FILE_PATH"
-    echo ""
-  } >> "$OUTPUT_FILE"
+    echo "File: $relative_path"
+    echo
+    cat "$filepath"
+    echo
+  } >> "$output_file"
 done
 
-echo "Generated file: $OUTPUT_FILE"
+echo "Generated file: $output_file"
